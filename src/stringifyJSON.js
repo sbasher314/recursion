372cdfc779a6474fa8bf9a040f22a9f8424b8d37
sbasher314 <sbasher314@gmail.com>
2020-11-19 09:13:51 -0500

    Created rudimentary JSON Stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log(obj);
+  var stringify = ``;
+  if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'object') {
+    var isArray = Array.isArray(obj);
+    stringify += (isArray)? `[` : `{`;
+    var firstTime = true;
+    for (var key in obj) {
+      var value = obj[key];
+      var formatted = '';
+      if (!_.isFunction(value) ) {
+        console.log(key, value);
+        if (!isArray) {
+          formatted += `"${key}":`;
+        }
+        if (typeof value === 'number') {
+          formatted += `${value}`;
+        } else if (typeof value === 'object') {
+          formatted += `${stringifyJSON(value)}`;
+        } else if (typeof value == 'string') {
+          formatted += `"${value}"`;
+        } else {
+          formatted += value;
+        }
+        if (formatted !== '' && value !== undefined) {
+          stringify += ((firstTime)?'' :',') + formatted;
+        }
+        firstTime = false;
+      }
+    }
+    stringify += (isArray)? `]` : `}`;
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    stringify = `${obj}`;
+  } else if (typeof obj === 'string' || typeof obj === 'number') {
+    stringify = `"${obj}"`;
+  }
+
+  return stringify;
 };
