0b7fedf49be05898740fcd12b42e1562d1fd6bc0
sbasher314 <sbasher314@gmail.com>
2020-11-22 21:38:10 -0500

    Implemented 'getElementsByClassName' from scratch recursively

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,20 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  // your code here
+  var proper =  Array.prototype.slice.apply(document.getElementsByClassName(className));
+  var elements = [];
+  var getChildrenAsArray = function(element, className) {
+    element.classList = element.classList || [];
+    var classList = Array.prototype.slice.apply(element.classList);
+    var matches = (classList.includes(className)) ? [element] : []; // If current element has className, create new array with current elements as first index;
+    if (element.hasChildNodes() === true) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        var children = getChildrenAsArray(element.childNodes[i], className);
+        matches = matches.concat(children); // Recursively append (matching) children to the current list
+      }
+    }
+    return matches;
+  }
+  elements = getChildrenAsArray(document.body, className); // Gets all children and returns for those equaling className -- including first element passed.
+  return elements;
 };
